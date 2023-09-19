0a5151457f9694d10a24f29872ceb4583efca9b4
ikecabrido <russcabz28@gmail.com>
2023-09-19 15:16:54 +0800

    added content in other pages

--- src/components/pages/Courses.jsx
+++ src/components/pages/Courses.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import { Accordion, Button } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import "./Courses.css";
 
- const Courses = () => {
+const Courses = () => {
   return (
-    <div>Courses</div>
-  )
-}
-export default Courses
+    <section>
+      <div className="text">
+        <h1>Courses</h1>
+        <p>Build your Tech Career</p>
+      </div>
+      <div className="card">
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header></Accordion.Header>
+            <Accordion.Body>
+              <Container>
+                <Row>
+                  <Col>
+                    <Card style={{ width: "18rem" }}>
+                      <Card.Img
+                        variant="top"
+                        src="./src/assets/pics/cardimg1.jpg"
+                      />
+                      <Card.Body>
+                        <Card.Title>
+                          Full-Stack Web Development (Day Classes)
+                        </Card.Title>
+                        <Card.Text>
+                          Become a full-stack web developer with the
+                          fundamentals of both front-end and back-end web
+                          development. Enroll now!
+                        </Card.Text>
+                      </Card.Body>
+                      <Card.Footer className="text-muted">
+                        <p>32/40 enrolled</p>
+                        <Button>Learn more</Button>
+                      </Card.Footer>
+                    </Card>
+                  </Col>
+                  <Col>
+                    <Card style={{ width: "18rem" }}>
+                      <Card.Img
+                        variant="top"
+                        src="./src/assets/pics/cardimg1.jpg"
+                      />
+                      <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                          Some quick example text to build on the card title and
+                          make up the bulk of the card's content.
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                  <Col>
+                    <Card style={{ width: "18rem" }}>
+                      <Card.Img
+                        variant="top"
+                        src="./src/assets/pics/cardimg1.jpg"
+                      />
+                      <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                          Some quick example text to build on the card title and
+                          make up the bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                  <Col className="d-flex justify-content-center align-items-center">
+                    <Card style={{ width: "18rem" }}>
+                      <Card.Img
+                        variant="top"
+                        src="./src/assets/pics/cardimg1.jpg"
+                      />
+                      <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                          Some quick example text to build on the card title and
+                          make up the bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+              </Container>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+    </section>
+  );
+};
+export default Courses;
--- src/components/pages/Faqs.jsx
+++ src/components/pages/Faqs.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import Accordion from "react-bootstrap/Accordion";
+import "./faq.css";
 
 const Faqs = () => {
   return (
-    <div>Faqs</div>
-  )
-}
-export default Faqs
+    <section id="faq">
+      <div className="text-container">
+        <h1>Frequently Asked Questions</h1>
+        <p>
+          Hello! We have shared some of our frequently asked questions to help
+          you out.
+        </p>
+      </div>
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </section>
+  );
+};
+export default Faqs;
