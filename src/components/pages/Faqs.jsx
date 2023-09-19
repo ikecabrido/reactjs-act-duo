1209f3a66f4b3ce483ab31f3ac2c91750370165b
ikecabrido <russcabz28@gmail.com>
2023-09-19 15:25:11 +0800

    added some content

--- src/components/pages/Faqs.jsx
+++ src/components/pages/Faqs.jsx
@@ -11,32 +11,18 @@ const Faqs = () => {
           you out.
         </p>
       </div>
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+      <>
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>What is Kodego all about?</Accordion.Header>
+            <Accordion.Body>
+              KodeGo offers full-time online IT courses with a
+              study-now-pay-later setup, while providing end-to-end career
+              assistance and suport for our students after the bootcamp.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </>
     </section>
   );
 };
